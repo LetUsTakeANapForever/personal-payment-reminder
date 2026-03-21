f47d798125837e7c4d836607a6d975c176969ad1
LetUsTakeANapForever <kandamkankaw@gmail.com>
2026-03-21 18:55:49 +0700

    feat(auth-page): add google sign in, sign out strategy config and adjust auth UI page

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,43 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { admin } from "better-auth/plugins";
+import { nextCookies } from "better-auth/next-js";
+import { db } from "../db";
+import { user, session, account, verification } from "../db/schema";
+
+const adminRole = "admin";
+const userRole = "user";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      user: user,
+      session: session,
+      account: account,
+      verification: verification,
+    },
+  }),
+  baseURL: process.env.BETTER_AUTH_BASE_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      mapProfileToUser: (profile) => {
+        return {
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          role: userRole,
+        };
+      },
+    },
+  },
+  plugins: [
+    admin({
+      adminRoles: [adminRole],
+      defaultRole: userRole,
+    }),
+    nextCookies(),
+  ],
+});
