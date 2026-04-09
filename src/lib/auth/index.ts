4e713f281feec2bde10f9ba252c1a1d748871811
LetUsTakeANapForever <kandamkankaw@gmail.com>
2026-04-09 14:22:58 +0700

    feat(auth): fix incorrect profile name fetching, changed from username to display name

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -40,7 +40,7 @@ export const auth = betterAuth({
       permissions: 2048 | 16384, // Send msg and embeded links
       mapProfileToUser: (profile) => {
         return {
-          name: profile.username,
+          name: profile.global_name ?? undefined, // Display name
           email: profile.email,
           image: profile.avatar ? `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png` : undefined,
           role: userRole,
